60154157427590f62b2ab10fad005e19f8aa95a1
mohamadalbaaj <101297059+mohamadalbaaj@users.noreply.github.com>
2022-03-31 02:51:39 +0200

    Update Find_and_Draw_Contours_using_OpenCV_Python.py

--- Find_and_Draw_Contours_using_OpenCV_Python.py
+++ Find_and_Draw_Contours_using_OpenCV_Python.py
@@ -1,17 +1,21 @@
+
+
+
 import numpy as np
 import cv2
 
-img = cv2.imread('baseball.png')
+img = cv2.imread('OpenCV_Logo.png')
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(imgray, 127, 255, 0)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 print("Number of contours = " + str(len(contours)))
 print(contours[0])
 
-cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
-cv2.drawContours(imgray, contours, -1, (0, 255, 0), 3)
+final_image = cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
+cv2.drawContours(imgray, contours, 0, (0, 255, 0), 3)
 
+cv2.imwrite('image with countours.png', final_image)
 cv2.imshow('Image', img)
-cv2.imshow('Image GRAY', imgray)
+cv2.imshow('Image Gray', imgray)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
